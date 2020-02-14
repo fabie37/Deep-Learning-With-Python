e785567ffe96dbef165fd20c5c4e855b93947969
Fabrizio Catinella <fabiecatinella@gmail.com>
2020-02-14 21:19:21 +0000

    Venv setup and network working!

--- Chapter 2/first_network.py
+++ Chapter 2/first_network.py
@@ -18,3 +18,22 @@ network.add(layers.Dense(10, activation='softmax'))
 network.compile(optimizer="rmsprop",
                 loss="categorical_crossentropy",
                 metrics=['accuracy'])
+
+# Preparing Image Data
+train_images = train_images.reshape((60000, 28 * 28))
+train_images = train_images.astype('float32') / 255
+
+test_images = test_images.reshape((10000, 28 * 28))
+test_images = test_images.astype('float32') / 255
+
+# Preparing the labels
+from keras.utils import to_categorical
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
+
+# Testing network
+network.fit(train_images, train_labels,epochs=5,batch_size=128)
+test_loss, test_acc = network.evaluate(test_images, test_labels)
+print('Test_Acc:', test_acc)
+
